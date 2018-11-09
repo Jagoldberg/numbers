7c5c25221c89a91414e6812d4194525dd806b5ae
Jackson Goldberg <jagoldberg@chapman.edu>
2018-11-09 11:50:55 -0800

    Adds numbers.sh

---
+++ numbers.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+#numbers.sh
+#Jackson Goldberg
+echo "Enter a positive integer: "
+read val
+$N = 1
+while $N -le $val
+do
+	if [$((N%2)) -eq 0]
+	then
+	   echo $N "even"
+	   N = $[$N+1]
+	else
+	   echo $N "odd"
+	   N = $[$N+1]
+	fi'
+done
