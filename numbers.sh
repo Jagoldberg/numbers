0d7b71ec36723fc2f10c8b57180608642267369c
Jackson Goldberg <jagoldberg@chapman.edu>
2018-11-16 11:27:26 -0800

    Updates numbers.sh

--- numbers.sh
+++ numbers.sh
@@ -4,14 +4,14 @@
 echo "Enter a positive integer: "
 read val
 N=1
-while $N -le $val
+while [ $N -le $val ]
 do
-	if [$((N%2)) -eq 0]
+	if [ $((N%2)) -eq 0 ]
 	then
 	   echo $N "even"
 	   N=$[$N+1]
 	else
 	   echo $N "odd"
 	   N=$[$N+1]
-	fi'
+	fi
 done
